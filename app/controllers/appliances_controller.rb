bbec3e799f191b96ab2a9d185a299c6038fadff4
Mickael Guitton <mickaelkael88@hotmail.fr>
2024-08-14 15:17:53 +0200

    update routes to resources plus nesting bookings, update aplliances controller cretae plus new, create new view and form partial

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -3,4 +3,14 @@ class AppliancesController < ApplicationController
   def show
     @appliance = Appliance.find(params[:id])
   end
+
+  def create
+    @appliance = Appliance.create(appliance_params)
+    @appliance.user = current_user
+    redirect_to appliance_path
+  end
+
+  def new
+    @appliance = Appliance.new
+  end
 end
